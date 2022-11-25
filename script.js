2c332f0910c3c99d22a13ffa88a05179ba48b74c
99Ace <ace.ang@trentglobal.edu.sg>
2022-11-25 07:03:16 +0000

    add change icon of marker to leaficon

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// #1 Set up Leaf icon template
+var LeafIcon = L.Icon.extend({
+  options: {
+    // set the icon X and Y
+    iconSize: [38, 95],
+    // set the shadowSize X and Y
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76],
+  },
+});
+
+// #2 Set up a leaf icon
+var greenIcon = new LeafIcon({
+  iconUrl: "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+  shadowUrl: "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+});
+
+// #3 Set the icon into the marker
+let singaporeMarker = L.marker(
+  [1.29, 103.85], // first index contains the lat/lng
+  {
+    // second index contains an object
+    icon: greenIcon, // pass the greenIcon setting to 'icon' key
+  }
+);
+
+// #4 add to the map
+singaporeMarker.addTo(map);
